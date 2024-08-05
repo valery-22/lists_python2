867dbb73e04e90f9f2eb678ffcca009a6bc716c0
Valery Hoyos <73263344+valery-22@users.noreply.github.com>
2024-08-05 14:31:06 -0500

    first commit

---
+++ add.py
@@ -0,0 +1,8 @@
+# Current plan for the world tour
+world_tour_countries = ["Italy", "France", "USA", "Brazil", "India", "China"]
+
+# TODO: Visit Japan before Italy
+world_tour_countries.insert(0,"Japan")
+# TODO: Visit Spain after all other countries
+world_tour_countries.append("Spain")
+print(world_tour_countries)
---
+++ remove-lists.py
@@ -0,0 +1,9 @@
+# The current planned countries to visit in the world tour
+world_tour_countries = ["Italy", "France", "USA", "Brazil", "India", "China"]
+
+# Adding new countries to visit at the end of the tour
+world_tour_countries.append("Australia")
+world_tour_countries.append("Spain")
+
+# print the updated tour plan.
+print(world_tour_countries)
